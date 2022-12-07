c173cb30fe38f48efb01d9ca0f7475bb87659351
Karen Shiloh Jolly <karenshilohj@gmail.com>
2022-12-06 16:00:35 -0800

    updates

--- contact-me.js
+++ contact-me.js
@@ -1,1 +1,28 @@
 // TODO
+
+const selectEl = document.getElementById('contact-kind');
+
+const showAppropriateResponse = function() {
+    if(selectEl.value === 'business')
+    {
+        alert('hi there i am job');
+    }
+    else 
+    {
+        alert('i am not job');
+    }
+}
+
+selectEl.addEventListener('change', showAppropriateResponse);
+// document.getElementById("contact-kind").onchange = function(e) {
+//     id = document.getElementById("contact-kind").value;
+//     alert("id: ", id);
+// }
+// function showJobs() {
+//     var x = document.getElementById("jobs");
+//     if (x.style.display === "none") {
+//       x.style.display = "block";
+//     } else {
+//       x.style.display = "none";
+//     }
+//   }
--- form-validation-custom.js
+++ form-validation-custom.js
@@ -1,1 +1,37 @@
 // TODO
+document.addEventListener("DOMContentLoaded", function() {
+    const firstName = document.getElementById("first-name");
+    const lastName = document.getElementById("last-name");
+    const email = document.getElementById("email");
+    const form = document.getElementById('form');
+
+
+    function checkValidation() {
+        let allValid = false; 
+        // check firstname >= 3
+        if(firstName.value.length > 3) {
+            firstName.validity.valid = false;
+            firstName.classList.add("invalid");
+            firstName.setCustomValidity('Your name is too short');
+            firstName.reportValidity();
+        }
+        else {
+            firstName.validity.valid = true; 
+            firstName.classList.remove("invalid");
+            allValid == true;
+        }
+    }
+
+
+    if(email.value.match(regex)) //continue
+
+
+    form.addEventListener('submit', (e) => {
+        checkValidation(e)
+    })
+
+    
+
+
+
+});
