b8f669fc988a970c143697a842b51ce4cd59972d
Karen Shiloh Jolly <karenshilohj@gmail.com>
2022-12-07 21:38:36 -0800

    Stopping point

--- contact-me.js
+++ contact-me.js
@@ -1,38 +1,46 @@
 // TODO
 
-const selectEl = document.getElementById('contact-kind');
-document.getElementById("jobs").style.display = "none";
-document.getElementById("tech").style.display = "none";
-
-const showAppropriateResponse = function() {
-    let jobs = document.getElementById("jobs");
-    let tech = document.getElementById("tech"); 
-    if(selectEl.value === 'business')
-    {
-        jobs.style.display = "block";
-        tech.style.display = "none";
-    }
-    else 
-    {
-        jobs.style.display = "none";
-        tech.style.display = "block";
-       
-    }
-}
-
-selectEl.addEventListener('change', showAppropriateResponse);
-
-
 document.addEventListener("DOMContentLoaded", function() {
-    const firstName = document.getElementById("first-name");
-    const lastName = document.getElementById("last-name");
+    console.log('wemade it here right');
+    const firstName = document.getElementById("name");
+    const comment = document.getElementById("comment");
     const email = document.getElementById("email");
-    const form = document.getElementById('form');
+    const form = document.querySelector('form');
 
-    function checkValidation() {
+    const selectEl = document.getElementById('contact-kind');
+    document.getElementById("jobs").style.display = "none";
+    document.getElementById("tech").style.display = "none";
+    
+    
+    
+    
+
+
+    const showAppropriateResponse = function() {
+        let jobs = document.getElementById("jobs");
+        let tech = document.getElementById("tech"); 
+        if(selectEl.value === 'business')
+        {
+            jobs.style.display = "block";
+            tech.style.display = "none";
+        }
+        else 
+        {
+            jobs.style.display = "none";
+            tech.style.display = "block";
+           
+        }
+    }
+
+    function checkValidation(e) {
+        console.log('did we ever get here');
         let allValid = false; 
+        let firstValid = false;
+        let commentValid = false;
+        let emailValid = false;
         // check firstname >= 3
-        if(firstName.value.length > 3) {
+        if(firstName.value.length < 3) 
+        {    
             firstName.validity.valid = false;
             firstName.classList.add("invalid");
             firstName.setCustomValidity('Your name is too short');
@@ -40,18 +48,53 @@ document.addEventListener("DOMContentLoaded", function() {
         }
         else {
             firstName.validity.valid = true; 
-            firstName.classList.remove("invalid");
-            allValid == true;
+            // firstName.classList.remove("invalid");
+            firstValid = true;
+        }
+
+        if(comment.value.length < 10)
+        {
+            comment.validity.valid = false;
+            comment.classList.add("invalid");
+            comment.setCustomValidity('Your last name is too short');
+            comment.reportValidity();
+        }
+
+        else {
+            comment.validity.valid = true;
+            commentValid = true;
+
+        }
+
+        if(email.value == null || (email.value && email.value.match(/\w+@\w+\.\w+/).length == 0) )
+        {
+            console.log('we got here');
+            email.validity.valid = false;
+            email.classList.add("invalid");
+            email.setCustomValidity('Your email is in incorrect format');
+            email.reportValidity();
+        }
+        else {
+            console.log(email.value.match(/\w+@\w+\.\w+/));
+            email.validity.valid = true;
+            emailValid = true;
+        }
+
+        allValid = firstValid && commentValid && emailValid;
+        console.log('firstvalid is', firstValid);
+        console.log('lastvalid is ', commentValid);
+        console.log('allvalid is ', allValid);
+        console.log('emailValid is ', emailValid);
+
+        if(!allValid) {
+            e.preventDefault();
         }
     }
 
-    //selectEl.addEventListener('change', showAppropriateResponse);
-
-    //if(email.value.match(regex)) //continue
-
-
+    selectEl.addEventListener('change', showAppropriateResponse);
     form.addEventListener('submit', (e) => {
         checkValidation(e)
     })
 
+    
 });
--- form-validation-custom.js
+++ form-validation-custom.js
@@ -3,13 +3,19 @@ document.addEventListener("DOMContentLoaded", function() {
     const firstName = document.getElementById("first-name");
     const lastName = document.getElementById("last-name");
     const email = document.getElementById("email");
-    const form = document.getElementById('form');
+    const form = document.querySelector('form');
 
 
-    function checkValidation() {
-        let allValid = false; 
+
+
+    function checkValidation(e) {
+        let allValid = true; 
+        let firstValid = false;
+        let lastValid = false;
+        let emailValid = false;
         // check firstname >= 3
-        if(firstName.value.length > 3) {
+        if(firstName.value.length < 3) 
+        {    
             firstName.validity.valid = false;
             firstName.classList.add("invalid");
             firstName.setCustomValidity('Your name is too short');
@@ -17,13 +23,50 @@ document.addEventListener("DOMContentLoaded", function() {
         }
         else {
             firstName.validity.valid = true; 
-            firstName.classList.remove("invalid");
-            allValid == true;
+            // firstName.classList.remove("invalid");
+            firstValid = true;
+        }
+
+        if(lastName.value.length < 3)
+        {
+            lastName.validity.valid = false;
+            lastName.classList.add("invalid");
+            lastName.setCustomValidity('Your last name is too short');
+            lastName.reportValidity();
+        }
+
+        else {
+            lastName.validity.valid = true;
+            lastValid = true;
+
+        }
+
+        if(email.value.match(/\w+@\w+\.\w+/).length == 0)
+        {
+            console.log('we got here');
+            email.validity.valid = false;
+            email.classList.add("invalid");
+            email.setCustomValidity('Your email is in incorrect format');
+            email.reportValidity();
+        }
+        else {
+            console.log(email.value.match(/\w+@\w+\.\w+/));
+            email.validity.valid = true;
+            emailValid = true;
+        }
+
+        allValid = firstValid && lastValid && emailValid;
+        console.log('firstvalid is', firstValid);
+        console.log('lastvalid is ', lastValid);
+        console.log('allvalid is ', allValid);
+        console.log('emailValid is ', emailValid);
+
+        if(!allValid) {
+            e.preventDefault();
         }
     }
 
 
-    if(email.value.match(regex)) //continue
 
 
     form.addEventListener('submit', (e) => {
